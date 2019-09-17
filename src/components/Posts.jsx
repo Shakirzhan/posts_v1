b9b50160ff778f6f68ec6d763fa9e1d0fd6bf390
Shakirzhan <samarbaev99@mail.ru>
2019-09-17 14:24:58 +0300

    v 2

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -17,21 +17,21 @@ let Posts = (props) => {
 
     function Element() {
         return posts.map(el =>
-            <div>
-            <div><b>Post:</b></div>
-                <div key={el.id} className="panel panel-default">
-                    <div className="panel-heading">{el.title}</div>
-                    <div className="panel-body">
-                        {el.body}
-                        <div><b>Comments:</b></div>
-                        {el.comments.map(ch =>
-                            <div key={ch.id} className="panel panel-primary">
-                                <div className="panel-heading">{ch.name}</div>
-                                <div className="panel-body">
-                                    {ch.body}
-                                </div>
-                            </div>)}
-                    </div>
+            <div key={el.id}>
+                <div key={el.id}><b>Post:</b></div>
+                    <div className="panel panel-default">
+                        <div className="panel-heading">{el.title}</div>
+                        <div className="panel-body">
+                            {el.body}
+                            <div><b>Comments:</b></div>
+                            {el.comments.map(ch =>
+                                <div key={ch.id} className="panel panel-primary">
+                                    <div className="panel-heading">{ch.name}</div>
+                                    <div className="panel-body">
+                                        {ch.body}
+                                    </div>
+                                </div>)}
+                        </div>
                 </div>
             </div>
         );
